5b87943b20db5ba7a212da83b92b1f7a99d87702
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-04-15 11:26:55 +0100

    fix: update _id values in SHORT_LINKS for consistency and clarity

--- apps/handler/SHORT_LINKS.ts
+++ apps/handler/SHORT_LINKS.ts
@@ -3,14 +3,14 @@
 export const SHORT_LINKS = Object.freeze([
 
 	{
-		_id: '1',
+		_id: 'test_simple',
 		destination: 'https://www.google.com',
 		path: '/cm',
 		title: '1',
 	},
 
 	{
-		_id: '1',
+		_id: 'test_params',
 		destination: 'https://www.cmetropolitana.pt/:param1/:param2',
 		path: '/stops/:param1/:param2',
 		title: '1',

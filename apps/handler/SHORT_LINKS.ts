4f52f5b27ea4dc335c5e318b0705b27a0cd31025
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-04-15 11:46:57 +0100

    fix: reorder SHORT_LINKS entries for consistency and clarity

--- apps/handler/SHORT_LINKS.ts
+++ apps/handler/SHORT_LINKS.ts
@@ -3,17 +3,15 @@
 export const SHORT_LINKS = Object.freeze([
 
 	{
-		_id: 'test_simple',
-		destination: 'https://www.google.com',
-		path: '/cm',
-		title: '1',
+		_id: 'pip',
+		destination: 'https://beta.carrismetropolitana.pt/pip/:id',
+		path: '/pip/:id',
 	},
 
 	{
-		_id: 'test_params',
-		destination: 'https://www.cmetropolitana.pt/:param1/:param2',
-		path: '/stops/:param1/:param2',
-		title: '1',
+		_id: 'test_simple',
+		destination: 'https://www.google.com',
+		path: '/cm',
 	},
 
 ]);
